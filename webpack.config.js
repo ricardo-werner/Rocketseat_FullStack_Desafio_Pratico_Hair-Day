eaa6d9e34724d2652db9d9a77874c2f857b6388c
Ricardo Werner <wernerdive@gmail.com>
2025-10-19 21:48:25 -0300

    build(webpack)webpack) adicionado a configuracao do babel para navegadores mais antigos e compatibilizacao com o projeto

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,18 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ],
+          },
+        },
+      },
     ],
   },
 }
