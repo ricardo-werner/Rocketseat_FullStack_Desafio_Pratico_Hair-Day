f2acd5d2a6fbe9d13c9b43b7fadf998492f6d2f2
Ricardo Werner <wernerdive@gmail.com>
2025-10-19 21:27:23 -0300

    build(webpack): adicionado a configuracao para adicionar a pasta dist os assets_svg do projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -25,6 +26,14 @@ module.exports = {
       template: path.resolve(__dirname, "index.html"),
       favicon: path.resolve("src", "assets", "scissors.svg"),
   }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist", "src/assets"),
+        },
+      ],
+    }),
   ],
 
   module: {
