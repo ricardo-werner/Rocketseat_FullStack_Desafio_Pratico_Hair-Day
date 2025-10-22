a6d0fd3b302cd91b8950b4edfc7da7fb5bda191f
Ricardo Werner <wernerdive@gmail.com>
2025-10-22 16:06:14 -0300

    feat(hours-click): adicionado a logica para inserir e remover a classe=hour-selected na li clicada

---
+++ src/modules/form/hours-click.js
@@ -0,0 +1,16 @@
+export function hoursClick() { 
+  const hours = document.querySelectorAll(".hour-available");
+
+  hours.forEach((available) => {
+    available.addEventListener("click", (selected) => {
+
+      // Remove a classe hour-selected de todas as li não selecionadas.
+      hours.forEach((hour) => {
+        hour.classList.remove("hour-selected");
+      })
+
+      // Adiciona a classe na li clicada.
+      selected.target.classList.add("hour-selected");
+    })
+  })
+}
