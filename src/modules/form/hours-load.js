a2dcac0ec52f0f4488de161eb9f5c34d238a457e
Ricardo Werner <wernerdive@gmail.com>
2025-10-22 11:01:31 -0300

    feat(hours-load): adicionado a logica para renderizar os horarios no aplicativo conforme horario atual

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,23 +1,33 @@
 import { openingHours } from "../../utils/opening-hours.js";
 import dayjs from "dayjs";
 
+const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) { 
-  const opening = openingHours.map((hours) => {
+  const opening = openingHours.map((hour) => {
     //Recupera somente a hora
-    const [schedulesHour] = hours.split(":");
-
+    const [schedulesHour] = hour.split(":");
 
     //Adiciona a hora na data e verificar se esa no passado.
     const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs());
 
     // Define se o horário está disponível
-
     return {
-      hours,
+      hour,
       available: isHourPast,
     }
   })
 
-  console.log(opening)
+
+// Renderizar os horários.
+opening.forEach(({ hour, available }) => { 
+  const li = document.createElement("li");
+  
+  li.classList.add("hour");
+  li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+  li.textContent = hour;
+  hours.append(li);
+
+})
 };
