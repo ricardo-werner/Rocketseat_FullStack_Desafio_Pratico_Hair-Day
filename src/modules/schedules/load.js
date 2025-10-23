5c9e53310a0e804d9f96fbb1ed87aab767fddb5c
Ricardo Werner <wernerdive@gmail.com>
2025-10-23 17:50:58 -0300

    feat(schedules_load): adicionado a logica de busca e exibicao do agendamento

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,19 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
+import { schedulesShow } from "../schedules/show.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 //Seleciona o input de data.
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtém a data do input
   const date = selectedDate.value;
+
+  // Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+
+  // Exibe os agendamentos
+  schedulesShow({ dailySchedules });
 
   // Renderiza as horas disponíveis
   hoursLoad({ date });
