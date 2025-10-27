36125685f77c570618e8791ca02840c4d146811c
Ricardo Werner <105825127+ricardo-werner@users.noreply.github.com>
2025-10-27 11:32:21 -0300

    chore(config): atualiza baseURL para API pública de deploy
    Updated the baseURL in apiConfig to point to the new API endpoint.

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,5 +1,7 @@
 export const apiConfig = {
-  baseURL: "http://localhost:3333",
-}
+  // Troque isto:
+  // baseURL: "http://localhost:3333",
 
-// `${apiConfig.baseURL}/profile `
+  // Por isto: (Use seu usuário e nome do novo repositório)
+  baseURL: "https://my-json-server.typicode.com/ricardo-werner/Rocketseat_FullStack_Desafio_Pratico_Hair-Day-API"
+}
